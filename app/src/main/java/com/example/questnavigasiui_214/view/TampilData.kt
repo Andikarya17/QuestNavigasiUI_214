7e6b225c7b160f78a8d9f19294ba13a3165103d8
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-06 13:17:16 +0700

    tambahkan Scaffold dan TopAppBar dengan judul halaman Tampil Data

--- app/src/main/java/com/example/questnavigasiui_214/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_214/view/TampilData.kt
@@ -4,6 +4,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.ui.res.stringResource
 import androidx.compose.material3.R
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -15,4 +22,23 @@ fun TampilData(
         Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Lainnya"),
         Pair(first = stringResource(id = R.string.alamat), second = "Yogyakarta")
     )
+    Scaffold(
+        modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.tampil),
+                        color = Color.White
+                    )
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700)
+                )
+            )
+        }
+    ) { isiRuang ->
+
+    }
+
 }
