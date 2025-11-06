058a88a0406ce886a7a166510480616d67d5ed75
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-06 13:47:59 +0700

    buat struktur dasar FormIsian dengan Scaffold dan TopAppBar

--- app/src/main/java/com/example/questnavigasiui_214/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_214/view/Formulir.kt
@@ -1,2 +1,40 @@
 package com.example.questnavigasiui_214.view
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.questnavigasiui_214.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormIsian(
+    onSubmitClick: () -> Unit
+) {
+    Scaffold(
+        modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.home),
+                        color = Color.White
+                    )
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700)
+                )
+            )
+        }
+    ) { isiRuang ->
+
+    }
+}
