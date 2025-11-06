79b35cab01ec201556245b677c0cd339909861c8
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-06 13:57:41 +0700

    ngurangin import yg ga penting

--- app/src/main/java/com/example/questnavigasiui_214/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_214/view/Formulir.kt
@@ -1,7 +1,6 @@
 package com.example.questnavigasiui_214.view
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -9,9 +8,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.questnavigasiui_214.R
 
 @OptIn(ExperimentalMaterial3Api::class)
