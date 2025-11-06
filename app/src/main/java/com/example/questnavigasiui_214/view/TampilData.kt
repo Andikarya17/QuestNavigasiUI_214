f47d95b430fbe5a495667231ad1c76d4b0623ab7
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-06 13:35:28 +0700

    perbaiki codingan di button, dan import yang belum di import

--- app/src/main/java/com/example/questnavigasiui_214/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_214/view/TampilData.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.ui.res.stringResource
-import androidx.compose.material3.R
+import com.example.questnavigasiui_214.R
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.compose.material3.TopAppBar
@@ -82,7 +82,8 @@ fun TampilData(
                     modifier = Modifier.fillMaxWidth(),
                     onClick = onBackBtnClick
                 ) {
-                    Text(text = stringResource("Back"))
+                    Text(text = stringResource(id = R.string.back))
+
                 }
             }
         }
