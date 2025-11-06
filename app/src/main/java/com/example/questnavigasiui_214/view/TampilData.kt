e0a68d5689d4f0bc06024079dc25522bc2b4025b
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-06 13:08:38 +0700

    buat fungsi composable TampilData dengan anotasi ExperimentalMaterial3Api

---
+++ app/src/main/java/com/example/questnavigasiui_214/view/TampilData.kt
@@ -0,0 +1,12 @@
+package com.example.questnavigasiui_214.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.ExperimentalMaterial3Api
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+    onBackBtnClick: () -> Unit
+) {
+
+}
